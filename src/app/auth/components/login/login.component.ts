f0256195858e5b8d5bad87c077e2f75fa314c612
yulissateran <yulissa.lteran@gmail.com>
2020-02-08 11:40:39 -0500

    add firebase ath to register

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,34 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
-import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
+import { AngularFireAuth } from "@angular/fire/auth";
+import { auth } from "firebase/app";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  public formLogin: FormGroup;
 
-  constructor(public _angularFire: AngularFireAuth, private _router: Router) {
+  constructor(
+    public _angularFire: AngularFireAuth,
+    private _router: Router,
+    private _fb: FormBuilder
+  ) {
+    this.formLogin = this._fb.group({
+      email: [null, Validators.required],
+      password: [null, Validators.required]
+    });
   }
-  
+
   login() {
-    console.log( this._angularFire);
+    console.log(this._angularFire);
     event.preventDefault();
     // this._angularFire.auth.signInWithPopup(new auth.GoogleAuthProvider());
-    this._router.navigate(['admin/create-users']);
+    this._router.navigate(["admin/create-users"]);
 
     // this._router.navigate(['/home']);
-    console.log('redirect');
+    console.log("redirect");
   }
 
   logout() {
     // this.auth.signOut();
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
