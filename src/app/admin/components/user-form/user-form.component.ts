ebd7c8e2933081833d77090a5707702098e51f3c
yulissateran <yulissa.lteran@gmail.com>
2020-02-08 14:42:39 -0500

    create userForm

--- src/app/admin/components/user-form/user-form.component.ts
+++ src/app/admin/components/user-form/user-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { passwordValidator } from 'src/app/core/validators/password.validator';
 
 @Component({
   selector: 'app-user-form',
@@ -6,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
+public userForm: FormGroup;
+  constructor(private _fb : FormBuilder) { 
+    this.userForm = this._fb.group({
+      userName: [null, Validators.required],
+      password: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          passwordValidator
+        ])
+      ],
+      fullName:[null,  Validators.compose([Validators.required, Validators.minLength(10)])],
+      enable: [true, Validators.required],
+      email: null,
+      adress: null,
 
-  constructor() { }
+    });
+  }
+      // userName:string;
+      // password: string,
+      // fullName: string,
+      // state: string;
+      // email?:string,
+      // adress?: string,
 
+  AddUser(){
+    event.preventDefault();
+    if(this.userForm.valid){
+      console.log('user: ', this.userForm.value);
+      
+    }
+  }
   ngOnInit() {
   }
 
---
+++ src/app/core/models/user-u.ts
@@ -0,0 +1,8 @@
+export interface User {
+  userName:string;
+  password: string,
+  fullName: string,
+  state: string;
+  email?:string,
+  adress?: string,
+}
