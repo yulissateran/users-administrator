46940a9ea5d8188ce8b4f4261470df9328a0fe58
Yulissa Teran Cerquín <yteran@ebizlatin.com>
2020-02-10 16:50:47 -0500

    from

--- src/app/admin/components/form/form.component.ts
+++ src/app/admin/components/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { passwordValidator } from 'src/app/core/validators/password.validator';
 
 @Component({
   selector: 'app-form',
@@ -6,8 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+form:FormGroup;
+constructor(private  _fb:FormBuilder){
+this.form = this.buildForm();
+}
+buildForm(){
+  return this._fb.group({
+    userName: [null, Validators.required],
+    password: [
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        passwordValidator
+      ])
+    ],
+    fullName: [
+      null,
+      Validators.compose([Validators.required, Validators.minLength(10)])
+    ],
+    enabled: [true, Validators.required],
+    email: null,
+    adress: null
+  });
+}
+// userName:string;
+// password: string,
+// fullName: string,
+// state: string;
+// email?:string,
+// adress?: string,
 
-  constructor() { }
 
   ngOnInit() {
   }
--- src/app/admin/pages/dashboard/dashboard.component.ts
+++ src/app/admin/pages/dashboard/dashboard.component.ts
@@ -149,32 +149,3 @@ export class DashboardComponent implements OnInit {
 
   }
 }
-
-// this.userForm = this.buildForm();
-
-// buildForm(){
-//   return this._fb.group({
-//     userName: [null, Validators.required],
-//     password: [
-//       null,
-//       Validators.compose([
-//         Validators.required,
-//         Validators.minLength(6),
-//         passwordValidator
-//       ])
-//     ],
-//     fullName: [
-//       null,
-//       Validators.compose([Validators.required, Validators.minLength(10)])
-//     ],
-//     enabled: [true, Validators.required],
-//     email: null,
-//     adress: null
-//   });
-// }
-// userName:string;
-// password: string,
-// fullName: string,
-// state: string;
-// email?:string,
-// adress?: string,
