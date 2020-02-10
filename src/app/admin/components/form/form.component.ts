584677f61322b25550681ad7bf1d7cf3afac1cbe
Yulissa Teran Cerquín <yteran@ebizlatin.com>
2020-02-10 17:01:52 -0500

    from

--- src/app/admin/components/form/form.component.ts
+++ src/app/admin/components/form/form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { passwordValidator } from 'src/app/core/validators/password.validator';
+import { User } from 'src/app/core/models/user';
 
 @Component({
   selector: 'app-form',
@@ -8,39 +9,44 @@ import { passwordValidator } from 'src/app/core/validators/password.validator';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-form:FormGroup;
-constructor(private  _fb:FormBuilder){
-this.form = this.buildForm();
-}
-buildForm(){
-  return this._fb.group({
-    userName: [null, Validators.required],
-    password: [
-      null,
-      Validators.compose([
-        Validators.required,
-        Validators.minLength(6),
-        passwordValidator
-      ])
-    ],
-    fullName: [
-      null,
-      Validators.compose([Validators.required, Validators.minLength(10)])
-    ],
-    enabled: [true, Validators.required],
-    email: null,
-    adress: null
-  });
-}
-// userName:string;
-// password: string,
-// fullName: string,
-// state: string;
-// email?:string,
-// adress?: string,
+  form: FormGroup;
+
+  @Output() customSubmit: EventEmitter<User> = new EventEmitter()
+  constructor(private _fb: FormBuilder) {
+    this.form = this.buildForm();
+  }
+  buildForm() {
+    return this._fb.group({
+      userName: [null, Validators.required],
+      password: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          passwordValidator
+        ])
+      ],
+      fullName: [
+        null,
+        Validators.compose([Validators.required, Validators.minLength(10)])
+      ],
+      enabled: [true, Validators.required],
+      email: null,
+      adress: null
+    });
+  }
+  // userName:string;
+  // password: string,
+  // fullName: string,
+  // state: string;
+  // email?:string,
+  // adress?: string,
 
 
   ngOnInit() {
+  }
+  validateSubmit() {
+    if (this.form.valid) this.customSubmit.emit(this.form.value);
   }
 
 }
